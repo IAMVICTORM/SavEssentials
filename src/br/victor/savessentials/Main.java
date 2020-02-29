1cc5b4f23b0f6aac22bd44c7ebd5d84ff467dbbb
MR KAISER VICTOR <leefaker56@gmail.com>
2020-02-29 04:21:34 -0300

    Atualizado Main

--- src/br/victor/savessentials/Main.java
+++ src/br/victor/savessentials/Main.java
@@ -3,7 +3,9 @@ package br.victor.savessentials;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import br.victor.saveessentials.commandmanager.CommandManager;
+import br.victor.savessentials.commands.BroadcastMessageCommand;
 import br.victor.savessentials.commands.HealthCommand;
+import br.victor.savessentials.commands.KillCommand;
 
 public class Main extends JavaPlugin {
 	CommandManager cm = new CommandManager();
@@ -13,17 +15,24 @@ public class Main extends JavaPlugin {
 	}
 	
 	public void onEnable() {
+		
 		instance = this;
-		try {
-			cm.addCommand(new HealthCommand(), this);
-			
-		} catch (ReflectiveOperationException e) {
-			e.printStackTrace();
-		}
+		registerCommands();
 		System.out.println("Plugin carregado com sucesso.");
 	}
 	
 	public void onDisable() {
 	System.out.println("Plugin descarregado com sucesso.");
+	}
+	
+	void registerCommands() {
+		try {
+			cm.addCommand(new HealthCommand(), this);
+			cm.addCommand(new KillCommand(), this);
+			cm.addCommand(new BroadcastMessageCommand(), this);
+			
+		} catch (ReflectiveOperationException e) {
+			e.printStackTrace();
+		}
 	}
 }
